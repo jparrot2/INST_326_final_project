275475b24299e21554ed398f095dc57b60200ca5
alex-britton <alexxb@terpmail.umd.edu>
2024-11-06 16:04:32 -0500

    name changed

---
+++ rummy.py
@@ -0,0 +1,14 @@
+class Reservation:
+    def __init__(self): 
+        pass 
+    
+class customer: 
+    pass 
+
+print("hello")
+print("alex edited file")
+
+class Kithcen:
+    def __init__(self, kitchen):
+        self.kitchen = kitchen
+print("Hi")
