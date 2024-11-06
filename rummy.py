6ca040043ba52dca354c3a7fcf6702eefcc3a57a
alex-britton <alexxb@terpmail.umd.edu>
2024-11-06 17:09:33 -0500

    hi

--- rummy.py
+++ rummy.py
@@ -2,17 +2,17 @@
 
 class Card: 
     def __init__(self, rank, suit):
-        self.rank= rank
-        self.suit= suit
+        self.rank = rank
+        self.suit = suit
     def __str__(self):
         return f"{self.rank} of {self.suit}"
     def __eq__(self, other):
         if isinstance (other, Card):
-            return self.rank == other.rank and self.suit==other.suit
+            return self.rank == other.rank and self.suit == other.suit
         return False 
     
 class Deck:
 
 class Player:
-
+       
 class RummyGame:
