e84211ce5fcd93eec44f4ba236b60c482e209b51
Anna Carpenter <acarpen5@terpmail.umd.edu>
2024-11-06 16:38:54 -0500

    Anna deleted stuff

--- rummy.py
+++ rummy.py
@@ -1,11 +1,5 @@
 '''Plays the card game rummy'''
 
 class Player:
-    '''add players (up to 4) to play the game'''
     
-class Dealer:
-    '''going to be the computer'''
-    
-class Opponents(Player):
-    '''inherits the player'''
     
