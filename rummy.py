965e055baf7ebf67f0ebd5cda04bf32e2ebd7e65
Jayla Parrott <jparrot2@terpmail.umd.edu>
2024-11-06 17:17:40 -0500

    Jayla added a method

--- rummy.py
+++ rummy.py
@@ -12,6 +12,13 @@ class Card:
         return False 
     
 class Deck:
+    def __init__(self): 
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        # this is a list comprehension that involves an instance of the Card class to to couple together suits and ranks.
+        self.cards = [Card(rank, suit) for rank in ranks for suit in suits]
+        
+        
 
 class Player:
 
