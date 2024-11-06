3e137696b6eb450abcb8a766850056835338a737
Anna Carpenter <acarpen5@terpmail.umd.edu>
2024-11-06 17:01:33 -0500

    Anna and Alex created Card class

--- rummy.py
+++ rummy.py
@@ -1,6 +1,15 @@
 '''Plays the card game rummy'''
 
-class Card:
+class Card: 
+    def __init__(self, rank, suit):
+        self.rank= rank
+        self.suit= suit
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+    def __eq__(self, other):
+        if isinstance (other, Card):
+            return self.rank == other.rank and self.suit==other.suit
+        return False 
     
 class Deck:
 
