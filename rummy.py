6a89a62ce7cc85d5fe6f38fdd052318b29b6fd43
Anna Carpenter <acarpen5@terpmail.umd.edu>
2024-11-08 12:23:54 -0500

    Anna added docstrings to Card class

--- rummy.py
+++ rummy.py
@@ -1,12 +1,26 @@
 '''Plays the card game rummy'''
 
 class Card: 
+    '''represents a card with a rank and suit
+    Attributes: 
+        rank (str): rank of the card (2, 3, 4, ... 10, Jack, ... Ace)
+        suit (str): suit of the card (hearts, diamonds, spades, clubs)'''
     def __init__(self, rank, suit):
+        '''initializes a Card instance with a given rank and suit
+        Args: 
+            rank(str): rank of the card (see attributes in Card class)
+            suit (str): suit of the card (see attributes in Card class)'''
         self.rank = rank
         self.suit = suit
     def __str__(self):
+        '''string representation of a card
+        Returns: 
+            (str) string in the format 'rank of suit' '''
         return f"{self.rank} of {self.suit}"
     def __eq__(self, other):
+        '''checks if two cards are equal by comparing rank and suit
+        Returns: 
+            (bool) True if the cards match exactly, False otherwise '''
         if isinstance (other, Card):
             return self.rank == other.rank and self.suit == other.suit
         return False 
