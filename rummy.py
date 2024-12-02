fad6804ff0349172a9e1eb02d29a785267980d49
Anna Carpenter <acarpen5@terpmail.umd.edu>
2024-12-02 18:01:47 -0500

    anna changed to is-set and is-run

--- rummy.py
+++ rummy.py
@@ -104,40 +104,31 @@ class Player:
         The cards are sorted first by rank and then by suit.
         """
         self.hand.sort(key=lambda x: (ranks.index(x.rank), suits.index(x.suit)))
-    def check_valid_hand(self):
-        def is_run(cards):
-            if len(cards) < 3:
+    def is_run(cards):
+        if len(cards) < 3:
+            return False
+        first_suit = cards[0].suit
+        for card in cards:
+            if card.suit != first_suit:
                 return False
-            cards.sort(key=lambda x: ranks.index(x.rank))
-            for i in range(len(cards) - 1):
-                if ranks.index(cards[i + 1].rank) != ranks.index(cards[i].rank) + 1:
-                    return False
-            return True
-        def is_set(cards):
-            if len(cards) < 3 or len(cards) > 4:
-                return False
-            first_rank = cards[0].rank
-            return all(card.rank == first_rank for card in cards) and \
-               len(set(card.suit for card in cards)) == len(cards)
-        suit_groups = {}
-        rank_groups = {}
-        for card in self.hand:
-            if card.suit not in suit_groups:
-                suit_groups[card.suit] = []
-            suit_groups[card.suit].append(card)
-            if card.rank not in rank_groups:
-                rank_groups[card.rank] = []
-            rank_groups[card.rank].append(card)
-        for suit, cards in suit_groups.items():
-            if len(cards) >= 3:
-                if not is_run(cards):
-                    return False
-        for rank, cards in rank_groups.items():
-            if len(cards) >= 3:
-                if not is_set(cards):
-                    return False
+        cards.sort(key=lambda x: ranks.index(x.rank))  # Sort by rank
+        for i in range(len(cards) - 1):
+            if ranks.index(cards[i + 1].rank) != ranks.index(cards[i].rank) + 1:
+                return False 
         return True
-        
+    def is_set(cards):
+        if len(cards) < 3 or len(cards) > 4:
+            return False
+        first_rank = cards[0].rank
+        for card in cards:
+            if card.rank != first_rank:
+                return False
+        suits=[]
+        for card in cards:
+            if card.suit in suits:
+                return False 
+            suits.appened(card.suit)
+        return True   
     def declare_win(self):
         if self.check_valid_hand():
             result = f"{self.name} wins!"
