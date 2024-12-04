eeb34ac74a41511912d31d7bbbc11cfe0e6152cf
Jayla Parrott <jparrot2@terpmail.umd.edu>
2024-12-04 17:17:51 -0500

    imported argparse and sys to allow functionality for command line arguments

--- rummy.py
+++ rummy.py
@@ -1,5 +1,7 @@
 '''Plays the card game rummy'''
 import random
+from argparse import ArgumentParser
+import sys 
 
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
